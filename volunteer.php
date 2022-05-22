f20ba322678e31fdb8d7e6e30f8a573061346c06
jncerveza <93199006+jncerveza@users.noreply.github.com>
2022-05-22 11:51:52 +0800

    Add files via upload

---
+++ volunteer.php
@@ -0,0 +1,93 @@
+<html>
+<head>
+<link rel="stylesheet" href="stylesheet.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<title>
+Volunteer Sign-Up
+</title>
+</head>
+<body background="banner0.png">
+
+<!-- navigation bar -->
+<ul class="topnav">
+  <li><a href="home.html">Home</a></li>
+      <li><a href="catcommunity.html">Cat Community</a></li>
+  <li><a href="volunteer.php">Volunteer</a></li>
+  <li><a href="adoptables.php">Adoptables</a></li>
+  <li class="right"><a href="aboutus.php">About Us</a></li>
+</ul>
+
+<!-- banner -->
+<div class="banner">
+<p class="head"> Volunteer </p>
+</div>
+
+<!-- sign up sheet -->
+<form action="volunteerinfo.php" method="post">
+<fieldset> <legend> Sign-Up Sheet </legend>
+<div class="question">
+<p class="title">
+<label> Complete Name: (Last Name, First Name Middle Initial)
+<input type="text" name="ln">, <input type="text" name="fn"> <input type="text" name="mi">
+</label> 
+</p>
+</div>
+
+<div class="question">
+<p class="title">
+<label> Occupation:
+<input type="text" name="occupation">
+</label> 
+</p>
+</div>
+
+<div class="question">
+<p class="title">
+<label> Address:
+<input type="text" name="address">
+</label> 
+</p>
+</div>
+
+<div class="question">
+<p class="title">
+<label> Availability <br>
+To: <input type="time" name="time"> From: <input type="time" name="time">
+</label> 
+</p>
+</div>
+
+<div class="question">
+<p class="title">
+<label>
+Day of the Week: <input type="text" name="day"> 
+</label> 
+</p>
+</div>
+
+
+<div class="question">
+<p class="title">
+<label> Upload Valid ID:
+<input type="file" name="validID">
+</label> 
+</p>
+</div>
+
+<div class="question">
+<p class="title">
+<input class="submit" type="submit" name="submit">
+</p>
+</div>
+</fieldset>
+
+
+<!-- footer -->
+<div class="footer">
+  <p>Contact Us</p>
+  <p> <a href="https://www.facebook.com/catsoflegaspivillage" class="fa fa-facebook"></a> <a href="https://www.instagram.com/catsoflegaspivillage" class="fa fa-instagram"></a>		Contact Number</p>
+</div>
+</body>
+</html>
